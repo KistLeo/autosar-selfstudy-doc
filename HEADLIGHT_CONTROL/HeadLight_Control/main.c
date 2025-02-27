73d4117c99dce2dfb530ac17eccbaf48370b3ff9
KistLeo <dinh.nguyenkistnd0208@gmail.com>
2025-02-27 13:28:13 +0700

    update

---
+++ HEADLIGHT_CONTROL/HeadLight_Control/main.c
@@ -0,0 +1,17 @@
+#include "Os.h"        
+
+// void InitHW(void) {
+//     Mcal_Init();
+//     Com_Init();
+//     Nvm_Init();
+//     Watchdog_Init();
+// }
+
+int main(void) {
+    
+    
+    StartOS();  
+    while (1) {}
+
+    return 0;
+}
